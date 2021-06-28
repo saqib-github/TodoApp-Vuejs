751280efcf54bbabbfa52aae5abb76c57225d9b8
saqib <rsaqib2034@gmail.com>
2021-06-28 17:40:03 +0500

    All file added succesfully

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <Input/>
+</template>
+
+<script>
+import Input from './components/Input.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Input
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Input.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+      <input
+      type="text"
+      placeholder="Enter Text to add"
+      v-model="inputValue"
+    />
+    <button type="submit" @click="addvalue()">+</button>
+  </div>
+  <ul>
+    <li v-for="inputValue in inputvalueArray" :key="inputValue">
+      {{ inputValue }}
+       <button type="submit" @click="deletevalue(inputValue)">-</button>
+       <button type="submit" @click="editvalue(inputValue)">edit</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "Input",
+  data() {
+    return {
+      inputValue: "",
+      inputvalueArray: [],
+      disabled: false,
+      currentIndex: 0,
+    };
+  },
+  methods: {
+    addvalue() {
+      this.inputvalueArray.push(this.inputValue);
+      this.inputValue=" ";
+      this.currentIndex++;
+    },
+    deletevalue(index) {
+      // let index1 = this.inputvalueArray.indexOf(index);
+      this.currentIndex = this.inputvalueArray.indexOf(index);
+      this.inputvalueArray.splice(this.currentIndex, 1);
+      this.currentIndex--;
+    },
+    editvalue(index){
+     let index1 = this.inputvalueArray.indexOf(index);
+     this.inputValue=this.inputvalueArray[index1];
+
+    //  this.inputvalueArray.splice(index1, 1);
+    //  this.inputvalueArray.push(this.inputValue);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
